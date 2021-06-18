73a1bd366491c9b52e532f9ef0ee87b78bdfe9ae
HarshitParsai <harshit.parsai@inviz.ai>
2021-06-18 21:52:09 +0530

    Added working list endpoint

---
+++ app/recipy/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+
+from recipy import views
+
+router = DefaultRouter()
+router.register('tags', views.TagViewSet)
+
+app_name = 'recipy'
+
+urlpatterns = [
+    path('', include(router.urls))
+]
